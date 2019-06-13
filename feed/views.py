07937296bf4bef4da7a9048f6326fac0ba36d3a0
kzmrt <info.murata.kazuhiko@gmail.com>
2019-06-13 14:50:52 +0900

    feedparserによる処理に変更。

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
 import logging
 from django.views import generic
+import feedparser
 
 logger = logging.getLogger('development')
 
@@ -7,3 +8,16 @@ logger = logging.getLogger('development')
 class IndexView(generic.TemplateView):
 
     template_name = 'feed/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        url = 'https://headlines.yahoo.co.jp/rss/all-c_sci.xml'
+        feed = feedparser.parse(url)
+
+        context = {
+            'feed_title': feed['feed']['title'],
+            'entries': feed['entries'],
+        }
+
+        return context
